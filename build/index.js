bf226e17ddb9305a6e0cead66645579bebdf0feb
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-12-17 01:06:25 -0800

    fixed for babel6 and react-transform

--- build/index.js
+++ build/index.js
@@ -1,1 +1,330 @@
-"use strict";function _interopRequireDefault(t){return t&&t.__esModule?t:{"default":t}}Object.defineProperty(exports,"__esModule",{value:!0});var _reactAddons=require("react/addons"),_reactAddons2=_interopRequireDefault(_reactAddons),_moment=require("moment"),_moment2=_interopRequireDefault(_moment);exports["default"]=_reactAddons2["default"].createClass({displayName:"IdleTimer",propTypes:{timeout:_reactAddons2["default"].PropTypes.number,events:_reactAddons2["default"].PropTypes.arrayOf(_reactAddons2["default"].PropTypes.string),idleAction:_reactAddons2["default"].PropTypes.func,activeAction:_reactAddons2["default"].PropTypes.func,element:_reactAddons2["default"].PropTypes.oneOfType([_reactAddons2["default"].PropTypes.object,_reactAddons2["default"].PropTypes.string]),format:_reactAddons2["default"].PropTypes.string},getDefaultProps:function(){return{timeout:12e5,events:["mousemove","keydown","wheel","DOMMouseScroll","mouseWheel","mousedown","touchstart","touchmove","MSPointerDown","MSPointerMove"],idleAction:function(){},activeAction:function(){},element:document}},getInitialState:function(){return{idle:!1,oldDate:+new Date,lastActive:+new Date,remaining:null,tId:null,pageX:null,pageY:null}},componentWillMount:function(){var t=this;this.props.events.forEach(function(e){t.props.element.addEventListener(e,t._handleEvent)})},componentWillUnmount:function(){var t=this;this.props.events.forEach(function(e){t.props.element.removeEventListener(e,t._handleEvent)})},render:function(){return _reactAddons2["default"].createElement("div",null,this.props.children?this.props.children:"")},_toggleIdleState:function(){this.setState({idle:!this.state.idle}),this.state.idle?this.props.idleAction():this.props.activeAction()},_handleEvent:function(t){if(!this.state.remaining){if("mousemove"===t.type){if(t.pageX===this.state.pageX&&t.pageY===this.state.pageY)return;if("undefined"==typeof t.pageX&&"undefined"==typeof t.pageY)return;var e=+new Date-this.state.oldDate;if(200>e)return}clearTimeout(this.state.tId),this.state.idle&&this._toggleIdleState(t),this.setState({lastActive:+new Date,pageX:t.pageX,pageY:t.pageY,tId:setTimeout(this._toggleIdleState,this.props.timeout)})}},reset:function(){clearTimeout(this.state.tId),this.setState({idle:!1,oldDate:+new Date,lastActive:this.state.oldDate,remaining:null,tId:setTimeout(this._toggleIdleState,this.props.timeout)})},pause:function(){null===this.state.remaining&&(clearTimeout(this.state.tId),this.setState({remaining:this.props.timeout-(+new Date-this.state.oldDate)}))},resume:function(){null!==this.state.remaining&&(this.state.idle||this.setState({tId:setTimeout(this._toggleIdleState,this.state.remaining),remaining:null}))},getRemainingTime:function(){if(this.state.idle)return 0;if(null!=this.state.remaining)return this.state.remaining;var t=this.props.timeout-(+new Date-this.state.lastActive);return 0>t&&(t=0),t},getElapsedTime:function(){return+new Date-this.state.oldDate},getLastActiveTime:function(){return this.props.format?_moment2["default"](this.state.lastActive).format(this.props.format):this.state.lastActive},isIdle:function(){return this.state.idle}}),module.exports=exports["default"];
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _redboxReact2 = require('redbox-react');
+
+var _redboxReact3 = _interopRequireDefault(_redboxReact2);
+
+var _reactTransformCatchErrors3 = require('react-transform-catch-errors');
+
+var _reactTransformCatchErrors4 = _interopRequireDefault(_reactTransformCatchErrors3);
+
+var _react2 = require('react');
+
+var _react3 = _interopRequireDefault(_react2);
+
+var _reactTransformHmr3 = require('react-transform-hmr');
+
+var _reactTransformHmr4 = _interopRequireDefault(_reactTransformHmr3);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _lodash = require('lodash.bindall');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _components = {
+  IdleTimer: {
+    displayName: 'IdleTimer'
+  }
+};
+
+var _reactTransformHmr2 = (0, _reactTransformHmr4.default)({
+  filename: 'src/index.js',
+  components: _components,
+  locals: [module],
+  imports: [_react3.default]
+});
+
+var _reactTransformCatchErrors2 = (0, _reactTransformCatchErrors4.default)({
+  filename: 'src/index.js',
+  components: _components,
+  locals: [],
+  imports: [_react3.default, _redboxReact3.default]
+});
+
+function _wrapComponent(id) {
+  return function (Component) {
+    return _reactTransformHmr2(_reactTransformCatchErrors2(Component, id), id);
+  };
+} /**
+   * React Idle Timer
+   *
+   * @author  Randy Lebeau
+   * @class   IdleTimer
+   *
+   */
+
+var IdleTimer = _wrapComponent('IdleTimer')((function (_Component) {
+  _inherits(IdleTimer, _Component);
+
+  function IdleTimer(props) {
+    _classCallCheck(this, IdleTimer);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(IdleTimer).call(this, props));
+
+    _this.state = { idle: false,
+      oldDate: +new Date(),
+      lastActive: +new Date(),
+      remaining: null,
+      tId: null,
+      pageX: null,
+      pageY: null
+    };
+
+    (0, _lodash2.default)(_this, ['_toggleIdleState', '_handleEvent', 'reset', 'pause', 'resume', 'getRemainingTime', 'getElapsedTime', 'getLastActiveTime', 'isIdle']);
+    return _this;
+  }
+
+  _createClass(IdleTimer, [{
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      var _this2 = this;
+
+      this.props.events.forEach(function (e) {
+        return _this2.props.element.addEventListener(e, _this2._handleEvent);
+      });
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      var _this3 = this;
+
+      this.props.events.forEach(function (e) {
+        return _this3.props.element.removeEventListener(e, _this3._handleEvent);
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react3.default.createElement(
+        'div',
+        null,
+        this.props.children ? this.props.children : ''
+      );
+    }
+
+    /////////////////////
+    // Private Methods //
+    /////////////////////
+
+    /**
+     * Toggles the idle state and calls the proper action
+     *
+     * @return {void}
+     *
+     */
+
+  }, {
+    key: '_toggleIdleState',
+    value: function _toggleIdleState() {
+      // Set the state
+      this.setState({ idle: !this.state.idle });
+
+      // Fire the appropriate action
+      if (!this.state.idle) this.props.activeAction();else this.props.idleAction();
+    }
+
+    /**
+     * Event handler for supported event types
+     *
+     * @param  {Object} e event object
+     * @return {void}
+     *
+     */
+
+  }, {
+    key: '_handleEvent',
+    value: function _handleEvent(e) {
+
+      // Already idle, ignore events
+      if (this.state.remaining) return;
+
+      // Mousemove event
+      if (e.type === 'mousemove') {
+        // if coord are same, it didn't move
+        if (e.pageX === this.state.pageX && e.pageY === this.state.pageY) return;
+        // if coord don't exist how could it move
+        if (typeof e.pageX === 'undefined' && typeof e.pageY === 'undefined') return;
+        // under 200 ms is hard to do, and you would have to stop, as continuous activity will bypass this
+        var elapsed = +new Date() - this.state.oldDate;
+        if (elapsed < 200) return;
+      }
+
+      // clear any existing timeout
+      clearTimeout(this.state.tId);
+
+      // if the idle timer is enabled, flip
+      if (this.state.idle) this._toggleIdleState(e);
+
+      this.setState({ lastActive: +new Date() // store when user was last active
+        , pageX: e.pageX // update mouse coord
+        , pageY: e.pageY,
+        tId: setTimeout(this._toggleIdleState, this.props.timeout) // set a new timeout
+      });
+    }
+
+    ////////////////
+    // Public API //
+    ////////////////
+
+    /**
+     * Restore initial settings and restart timer
+     *
+     * @return {Void}
+     *
+     */
+
+  }, {
+    key: 'reset',
+    value: function reset() {
+      // reset timers
+      clearTimeout(this.state.tId);
+
+      // reset settings
+      this.setState({ idle: false,
+        oldDate: +new Date(),
+        lastActive: this.state.oldDate,
+        remaining: null,
+        tId: setTimeout(this._toggleIdleState, this.props.timeout)
+      });
+    }
+
+    /**
+     * Store remaining time and stop timer.
+     * You can pause from idle or active state.
+     *
+     * @return {Void}
+     *
+     */
+
+  }, {
+    key: 'pause',
+    value: function pause() {
+      // this is already paused
+      if (this.state.remaining !== null) return;
+
+      // clear any existing timeout
+      clearTimeout(this.state.tId);
+
+      // define how much is left on the timer
+      this.setState({ remaining: this.props.timeout - (+new Date() - this.state.oldDate) });
+    }
+
+    /**
+     * Resumes a stopped timer
+     *
+     * @return {Void}
+     *
+     */
+
+  }, {
+    key: 'resume',
+    value: function resume() {
+      // this isn't paused yet
+      if (this.state.remaining === null) return;
+
+      // start timer and clear remaining
+      if (!this.state.idle) {
+        this.setState({ tId: setTimeout(this._toggleIdleState, this.state.remaining),
+          remaining: null
+        });
+      }
+    }
+
+    /**
+     * Time remaining before idle
+     *
+     * @return {Number} Milliseconds remaining
+     *
+     */
+
+  }, {
+    key: 'getRemainingTime',
+    value: function getRemainingTime() {
+      // If idle there is no time remaining
+      if (this.state.idle) return 0;
+
+      // If its paused just return that
+      if (this.state.remaining != null) return this.state.remaining;
+
+      // Determine remaining, if negative idle didn't finish flipping, just return 0
+      var remaining = this.props.timeout - (+new Date() - this.state.lastActive);
+      if (remaining < 0) remaining = 0;
+
+      // If this is paused return that number, else return current remaining
+      return remaining;
+    }
+
+    /**
+     * How much time has elapsed
+     *
+     * @return {Timestamp}
+     *
+     */
+
+  }, {
+    key: 'getElapsedTime',
+    value: function getElapsedTime() {
+      return +new Date() - this.state.oldDate;
+    }
+
+    /**
+     * Last time the user was active
+     *
+     * @return {Timestamp}
+     *
+     */
+
+  }, {
+    key: 'getLastActiveTime',
+    value: function getLastActiveTime() {
+      if (this.props.format) return (0, _moment2.default)(this.state.lastActive).format(this.props.format);
+      return this.state.lastActive;
+    }
+
+    /**
+     * Is the user idle
+     *
+     * @return {Boolean}
+     *
+     */
+
+  }, {
+    key: 'isIdle',
+    value: function isIdle() {
+      return this.state.idle;
+    }
+  }]);
+
+  return IdleTimer;
+})(_react2.Component));
+
+IdleTimer.propTypes = { timeout: _react2.PropTypes.number // Activity timeout
+  , events: _react2.PropTypes.arrayOf(_react2.PropTypes.string) // Activity events to bind
+  , idleAction: _react2.PropTypes.func // Action to call when user becomes inactive
+  , activeAction: _react2.PropTypes.func // Action to call when user becomes active
+  , element: _react2.PropTypes.oneOfType([_react2.PropTypes.object, _react2.PropTypes.string]) // Element ref to watch activty on
+  , format: _react2.PropTypes.string
+};
+
+IdleTimer.defaultProps = { timeout: 1000 * 60 * 20 // 20 minutes
+  , events: ['mousemove', 'keydown', 'wheel', 'DOMMouseScroll', 'mouseWheel', 'mousedown', 'touchstart', 'touchmove', 'MSPointerDown', 'MSPointerMove'],
+  idleAction: function idleAction() {},
+  activeAction: function activeAction() {},
+  element: document
+};
+
+module.exports = IdleTimer;
+module.exports['default'] = IdleTimer;
--- examples/App.js
+++ examples/App.js
@@ -1,88 +1,205 @@
-import React from 'react';
-import IdleTimer from '../build/index';
+'use strict';
 
-export default React.createClass({
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-	displayName: 'App',
+var _redboxReact2 = require('redbox-react');
 
-	getInitialState() {
-		return {
-			timeout: 3000,
-			remaining: null,
-			isIdle: false,
-			lastActive: null,
-			elapsed: null
-		}
-	},
+var _redboxReact3 = _interopRequireDefault(_redboxReact2);
 
-	componentDidMount() {
-		this.setState({
-			remaining: this.refs.idleTimer.getRemainingTime(),
-			lastActive: this.refs.idleTimer.getLastActiveTime(),
-			elapsed: this.refs.idleTimer.getElapsedTime()
-		});
+var _reactTransformCatchErrors3 = require('react-transform-catch-errors');
 
-		setInterval(() => {
-			this.setState({
-				remaining: this.refs.idleTimer.getRemainingTime(),
-				lastActive: this.refs.idleTimer.getLastActiveTime(),
-				elapsed: this.refs.idleTimer.getElapsedTime()
-			});
-		}, 1000);
-	},
+var _reactTransformCatchErrors4 = _interopRequireDefault(_reactTransformCatchErrors3);
 
-	render() {
+var _react2 = require('react');
 
-		return(
-			<IdleTimer
-				ref="idleTimer"
-				activeAction={this._onActive}
-				idleAction={this._onIdle}
-				timeout={this.state.timeout}
-				format="MM-DD-YYYY HH:MM:ss.SSS">
+var _react3 = _interopRequireDefault(_react2);
 
-				<div>
-					<h1>Timeout: {this.state.timeout}ms</h1>
-					<h1>Time Remaining: {this.state.remaining}</h1>
-					<h1>Time Elapsed: {this.state.elapsed}</h1>
-					<h1>Last Active: {this.state.lastActive}</h1>
-					<h1>Idle: {this.state.isIdle.toString()}</h1>
-				</div>
+var _reactTransformHmr3 = require('react-transform-hmr');
 
-				<div>
-					<button onClick={this._reset}>RESET</button>
-					<button onClick={this._pause}>PAUSE</button>
-					<button onClick={this._resume}>RESUME</button>
-				</div>
+var _reactTransformHmr4 = _interopRequireDefault(_reactTransformHmr3);
 
-			</IdleTimer>
-		);
-	},
+var _index = require('../build/index');
 
-	_onActive() {
-		this.setState({ isIdle: false });
-	},
+var _index2 = _interopRequireDefault(_index);
 
-	_onIdle() {
-		this.setState({ isIdle: true });
-	},
+var _lodash = require('lodash.bindall');
 
-	_changeTimeout() {
-		this.setState({
-			timeout: this.refs.timeoutInput.state.value()
-		});
-	},
+var _lodash2 = _interopRequireDefault(_lodash);
 
-	_reset() {
-		this.refs.idleTimer.reset();
-	},
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	_pause() {
-		this.refs.idleTimer.pause();
-	},
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	_resume() {
-		this.refs.idleTimer.resume();
-	}
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _components = {
+  App: {
+    displayName: 'App'
+  }
+};
+
+var _reactTransformHmr2 = (0, _reactTransformHmr4.default)({
+  filename: 'src_examples/App.js',
+  components: _components,
+  locals: [module],
+  imports: [_react3.default]
 });
+
+var _reactTransformCatchErrors2 = (0, _reactTransformCatchErrors4.default)({
+  filename: 'src_examples/App.js',
+  components: _components,
+  locals: [],
+  imports: [_react3.default, _redboxReact3.default]
+});
+
+function _wrapComponent(id) {
+  return function (Component) {
+    return _reactTransformHmr2(_reactTransformCatchErrors2(Component, id), id);
+  };
+}
+
+var App = _wrapComponent('App')((function (_Component) {
+  _inherits(App, _Component);
+
+  function App(props) {
+    _classCallCheck(this, App);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(App).call(this, props));
+
+    _this.state = { timeout: 3000,
+      remaining: null,
+      isIdle: false,
+      lastActive: null,
+      elapsed: null
+    };
+    (0, _lodash2.default)(_this, ['_onActive', '_onIdle', '_changeTimeout', '_reset', '_pause', '_resume']);
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.setState({
+        remaining: this.refs.idleTimer.getRemainingTime(),
+        lastActive: this.refs.idleTimer.getLastActiveTime(),
+        elapsed: this.refs.idleTimer.getElapsedTime()
+      });
+
+      setInterval(function () {
+        _this2.setState({
+          remaining: _this2.refs.idleTimer.getRemainingTime(),
+          lastActive: _this2.refs.idleTimer.getLastActiveTime(),
+          elapsed: _this2.refs.idleTimer.getElapsedTime()
+        });
+      }, 1000);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react3.default.createElement(
+        _index2.default,
+        {
+          ref: 'idleTimer',
+          activeAction: this._onActive,
+          idleAction: this._onIdle,
+          timeout: this.state.timeout,
+          format: 'MM-DD-YYYY HH:MM:ss.SSS' },
+        _react3.default.createElement(
+          'div',
+          null,
+          _react3.default.createElement(
+            'h1',
+            null,
+            'Timeout: ',
+            this.state.timeout,
+            'ms'
+          ),
+          _react3.default.createElement(
+            'h1',
+            null,
+            'Time Remaining: ',
+            this.state.remaining
+          ),
+          _react3.default.createElement(
+            'h1',
+            null,
+            'Time Elapsed: ',
+            this.state.elapsed
+          ),
+          _react3.default.createElement(
+            'h1',
+            null,
+            'Last Active: ',
+            this.state.lastActive
+          ),
+          _react3.default.createElement(
+            'h1',
+            null,
+            'Idle: ',
+            this.state.isIdle.toString()
+          )
+        ),
+        _react3.default.createElement(
+          'div',
+          null,
+          _react3.default.createElement(
+            'button',
+            { onClick: this._reset },
+            'RESET'
+          ),
+          _react3.default.createElement(
+            'button',
+            { onClick: this._pause },
+            'PAUSE'
+          ),
+          _react3.default.createElement(
+            'button',
+            { onClick: this._resume },
+            'RESUME'
+          )
+        )
+      );
+    }
+  }, {
+    key: '_onActive',
+    value: function _onActive() {
+      this.setState({ isIdle: false });
+    }
+  }, {
+    key: '_onIdle',
+    value: function _onIdle() {
+      this.setState({ isIdle: true });
+    }
+  }, {
+    key: '_changeTimeout',
+    value: function _changeTimeout() {
+      this.setState({
+        timeout: this.refs.timeoutInput.state.value()
+      });
+    }
+  }, {
+    key: '_reset',
+    value: function _reset() {
+      this.refs.idleTimer.reset();
+    }
+  }, {
+    key: '_pause',
+    value: function _pause() {
+      this.refs.idleTimer.pause();
+    }
+  }, {
+    key: '_resume',
+    value: function _resume() {
+      this.refs.idleTimer.resume();
+    }
+  }]);
+
+  return App;
+})(_react2.Component));
+
+module.exports = App;
+module.exports['default'] = App;
