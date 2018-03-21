9a6aeb0e4647bda53108f2e7388bd0cc0752f759
Vincent Taverna <vinnymac@gmail.com>
2018-03-21 15:05:59 -0400

    Fix case sensitive event type
    `mouseWheel` !== `mousewheel`

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export default class IdleTimer extends Component {
 
   static defaultProps = {
       timeout: 1000 * 60 * 20, // 20 minutes
-      events: ['mousemove', 'keydown', 'wheel', 'DOMMouseScroll', 'mouseWheel', 'mousedown', 'touchstart', 'touchmove', 'MSPointerDown', 'MSPointerMove'],
+      events: ['mousemove', 'keydown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'mousedown', 'touchstart', 'touchmove', 'MSPointerDown', 'MSPointerMove'],
       idleAction: () => {},
       activeAction: () => {},
       element: (typeof window === 'undefined' ? 'undefined' : typeof (window)) === 'object' ? document : {},
