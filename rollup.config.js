a0b01db17185c68c205e45342402403acbb492a3
Randy Lebeau <randylebeau@gmail.com>
2021-04-13 13:46:32 -0700

    🐞 Fix regenerator runtime bug.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
@@ -26,7 +27,11 @@ export default {
   ],
   plugins: [
     resolve(),
-    babel({ babelHelpers: 'bundled' }),
+    commonjs(),
+    babel({
+      babelHelpers: 'runtime',
+      skipPreflightCheck: true
+    }),
     terser()
   ]
 }
