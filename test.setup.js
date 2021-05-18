72756a7b371e00d2ac348a287a380b6a270d17b3
Randy Lebeau <randylebeau@gmail.com>
2021-05-18 16:28:13 -0700

    ✨ Update enzyme adapter for React 17.

--- test.setup.js
+++ test.setup.js
@@ -2,7 +2,7 @@
 import 'regenerator-runtime/runtime'
 import expect from 'expect'
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 configure({ adapter: new Adapter() })
 
