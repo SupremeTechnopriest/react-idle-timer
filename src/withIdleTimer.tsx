1d2106f9664774f7744cd47fd67c3efe60c89217
Randy Lebeau <randylebeau@gmail.com>
2022-12-26 22:03:36 -0500

    🐞 Fix this scope on higher order component.

--- src/withIdleTimer.tsx
+++ src/withIdleTimer.tsx
@@ -1,5 +1,9 @@
-import React, { ComponentType, forwardRef } from 'react'
-import { IIdleTimer, IIdleTimerProps, useIdleTimer } from '.'
+import type { ComponentType } from 'react'
+import type { IIdleTimer, IIdleTimerProps } from '.'
+import type { IEventHandler } from './types/IEventHandler'
+
+import React, { Component, forwardRef } from 'react'
+import { useIdleTimer } from '.'
 
 /**
  * Higher Order Component (HOC) for adding IdleTimer.
@@ -11,35 +15,7 @@ export function withIdleTimer<T extends IIdleTimer> (Component: ComponentType<T>
   type WithIdleTimerProps = Omit<T, keyof IIdleTimer> & IIdleTimerProps
   return forwardRef<IIdleTimer, WithIdleTimerProps>(function IdleTimer (props, ref) {
     const options = { ...props }
-
     const idleTimer = useIdleTimer(options)
-    const component = <Component {...(props as unknown as T)} {...idleTimer} />
-
-    if (!options.onPrompt && Component.prototype.onPrompt) {
-      options.onPrompt = Component.prototype.onPrompt.apply(component)
-    }
-
-    if (!options.onIdle && Component.prototype.onIdle) {
-      options.onIdle = Component.prototype.onIdle.apply(component)
-    }
-
-    if (!options.onActive && Component.prototype.onActive) {
-      options.onActive = Component.prototype.onActive.apply(component)
-    }
-
-    if (!options.onAction && Component.prototype.onAction) {
-      options.onAction = Component.prototype.onAction.call(component)
-    }
-
-    // Timeout is needed to allow hook to finish setting up.
-    const componentDidMount = Component.prototype.componentDidMount
-    if (componentDidMount) {
-      Component.prototype.componentDidMount = function () {
-        setTimeout(() => {
-          componentDidMount.call(component)
-        })
-      }
-    }
 
     if (typeof ref === 'function') {
       ref(idleTimer)
@@ -47,6 +23,60 @@ export function withIdleTimer<T extends IIdleTimer> (Component: ComponentType<T>
       ref.current = idleTimer
     }
 
-    return component
+    const componentDidMount = Component.prototype.componentDidMount
+    if (componentDidMount) {
+      Component.prototype.componentDidMount = function () {
+        setTimeout(() => {
+          componentDidMount.apply(this, arguments)
+        })
+      }
+    }
+
+    return <Component {...(props as unknown as T)} {...idleTimer} />
   })
+}
+
+interface IIdleTimerComponentProps extends IIdleTimerProps {
+  setOnPrompt: (fn: IEventHandler) => void
+  setOnIdle: (fn: IEventHandler) => void
+  setOnActive: (fn: IEventHandler) => void
+  setOnAction: (fn: IEventHandler) => void
+  setOnMessage: (fn: IEventHandler) => void
+}
+
+abstract class IIdleTimerComponent<P, S> extends Component<P, S> {
+  /**
+   * When promptTimeout is set, this function is called after the user becomes
+   * idle. This is useful for displaying a confirm prompt. If the prompt timeout
+   * is reached, onIdle is then called.
+   */
+  onPrompt? (): void
+  /**
+   * Function to call when user is idle.
+   */
+  onIdle? (): void
+  /**
+   * Function to call when user becomes active.
+   */
+  onActive? (event: Event): void
+  /**
+   * Function to call on user activity. Can be throttled or debounced using the
+   * `throttle` and `debounce` props.
+   */
+  onAction? (event: Event): void
+  /**
+   * Function to call when message is has been emitted.
+   */
+  onMessage? (data: any): void
+}
+
+export class IdleTimerComponent<P, S> extends IIdleTimerComponent<P, S> {
+  constructor (props: P & IIdleTimerComponentProps) {
+    super(props)
+    if (this.onPrompt) props.setOnPrompt(this.onPrompt.bind(this))
+    if (this.onIdle) props.setOnIdle(this.onIdle.bind(this))
+    if (this.onActive) props.setOnActive(this.onActive.bind(this))
+    if (this.onAction) props.setOnAction(this.onAction.bind(this))
+    if (this.onMessage) props.setOnMessage(this.onMessage.bind(this))
+  }
 }
